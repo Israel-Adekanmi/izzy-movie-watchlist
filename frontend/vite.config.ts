5c88821b8aefad38e67245509f1c45ea418628ba
Israel Adekanmi <adekanmiisrael8@gmail.com>
2024-11-26 11:50:49 +0100

    okay stuff

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: Number(process.env.PORT) || 4173,  // Default to 4173, but allow for deployment overrides
+    host: '0.0.0.0',  // Ensure that the server is listening on all interfaces
+    port: process.env.PORT || 4173,  // Use the provided PORT or default to 4173
   },
 });
